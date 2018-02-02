0e15cafa90a1a7656548dbccb0c5e6a409b0cae4
bhanukishore1 <bhanukishore.saineni@gmail.com>
2018-02-01 20:34:16 -0600

    sends email to tomcat

--- src/app/gmail.service.ts
+++ src/app/gmail.service.ts
@@ -1,12 +1,17 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import {HttpHeaders} from '@angular/common/http';
 
 
 @Injectable()
 export class GmailService {
+  
   constructor(private http: Http ) {}
   sendGmail(gmail: any ) {
     console.log('sending:' + gmail);
-    return this.http.post('https://emailsending-fd6e3.firebaseio.com/data.json', JSON.stringify({gmail}));
+
+    /*return this.http.post('https://emailsending-fd6e3.firebaseio.com/data.json', JSON.stringify({gmail}));*/
+    return this.http.post('http://localhost:8080/NewMavenProj/webapi/myresource', JSON.stringify({gmail}));
+    /*return this.http.get('http://localhost:8080/NewMavenProj/webapi/myresource/' + this.str  );*/
         }
 }
